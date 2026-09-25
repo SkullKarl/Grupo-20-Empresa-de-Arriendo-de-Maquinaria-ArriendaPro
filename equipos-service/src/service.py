ac53787b3eaa8de2dbfe1581071a51723f5c57fb
gustavol <gustavo.gonzalez8190@gmail.com>
2026-09-24 22:40:58 -0300

    El servicio de equipos ya levanta sin problemas

--- equipos-service/src/service.py
+++ equipos-service/src/service.py
@@ -10,7 +10,21 @@ from database import db, init_db
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 equipos_collection = db["equipos"]
 
-class EquipoService(equipo_pb2_grpc.EquipoService):
+class EquipoService(equipo_pb2_grpc.EquipoServiceServicer):
+
+    def GetEquipo(self, request, context):
+            eq = equipos_collection.find_one({"id": request.id})
+            if not eq:
+                context.abort(grpc.StatusCode.NOT_FOUND, f"El equipo con ID {request.id} no existe")
+            
+            equipo_msg = equipo_pb2.Equipo(
+                id=eq["id"],
+                name=eq["name"],
+                type=eq["type"],
+                total_units=eq["total_units"],
+                available_units=eq["available_units"]
+            )
+            return equipo_pb2.GetEquipoResponse(equipo=equipo_msg)
     
     def ListCatalog(self, request, context):
         equipos_list = []
@@ -23,8 +37,53 @@ class EquipoService(equipo_pb2_grpc.EquipoService):
                 available_units=eq["available_units"]
             ))
         return equipo_pb2.ListCatalogResponse(equipos=equipos_list)
+    
+    def ModifyReservation(self, request, context):
+        eq = equipos_collection.find_one({"id": request.equipment_id})
+        if not eq:
+            context.abort(grpc.StatusCode.NOT_FOUND, "Equipo no encontrado")
 
-def service():
+        if request.operation == equipo_pb2.RESERVE:
+            if eq["available_units"] >= request.quantity:
+                new_units = eq["available_units"] - request.quantity
+                equipos_collection.update_one(
+                    {"id": request.equipment_id},
+                    {"$set": {"available_units": new_units}}
+                )
+                return equipo_pb2.ModifyReservationResponse(
+                    success=True,
+                    message="Reserva realizada con éxito",
+                    current_available_units=new_units
+                )
+            else:
+                return equipo_pb2.ModifyReservationResponse(
+                    success=False,
+                    message="Stock insuficiente",
+                    current_available_units=eq["available_units"]
+                )
+
+        elif request.operation == equipo_pb2.RELEASE:
+            new_units = eq["available_units"] + request.quantity
+            if new_units > eq["total_units"]:
+                new_units = eq["total_units"]
+                
+            equipos_collection.update_one(
+                {"id": request.equipment_id},
+                {"$set": {"available_units": new_units}}
+            )
+            return equipo_pb2.ModifyReservationResponse(
+                success=True,
+                message="Equipo liberado correctamente",
+                current_available_units=new_units
+            )
+
+        return equipo_pb2.ModifyReservationResponse(
+            success=False,
+            message="Operación desconocida",
+            current_available_units=eq["available_units"]
+        )
+
+def server():
     for i in range(5):
         try:
             init_db()
@@ -33,12 +92,12 @@ def service():
             print(f"Esperando a MongoDB... intento {i+1}/5")
             time.sleep(3)
 
-    service = grpc.service(futures.ThreadPoolExecutor(max_workers=10))
-    equipo_pb2_grpc.add_EquipoService_to_service(EquipoService(), service)
-    service.add_insecure_port('[::]:50051')
-    service.start()
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    equipo_pb2_grpc.add_EquipoServiceServicer_to_server(EquipoService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
     print("Servidor gRPC de Equipos corriendo en el puerto 50051")
-    service.wait_for_termination()
+    server.wait_for_termination()
 
 if __name__ == '__main__':
-    service()
+    server()
